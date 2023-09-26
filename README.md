44e586c6bd47566932dccdaadbc1952f7ec2edaa
he3als <65787561+he3als@users.noreply.github.com>
2023-09-26 23:10:21 +0100

    feat!: update to v1.5.0

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
-# EdgeRemover
-PowerShell script to remove Microsoft Edge, which is based off of [ave9858's script](https://gist.github.com/ave9858/c3451d9f452389ac7607c99d45edecc6).
+# ❌ Edge Remover
+A PowerShell script that aims to non-forcefully remove Microsoft Edge in a user-friendly manner on Windows 10 and 11, based upon [ave9858's uninstallation method](https://gist.github.com/ave9858/c3451d9f452389ac7607c99d45edecc6).
 
-**Download:** https://github.com/he3als/EdgeRemover/releases/latest/download/RemoveEdge.ps1
+## ⬇️ Usage
+You can use this command in the Run dialog or Command Prompt for quick access. Alternatively, get the script from the GitHub releases.
 
-### For quick removal, use either of these commands in Run (Win + R)
-```powershell
-powershell iex """&{$(irm https://raw.githubusercontent.com/he3als/EdgeRemover/main/get.ps1)} -UninstallAll"""
-```
+For people that want to implement this in scripts, run `Get-Help .\RemoveEdge.ps1`. You can append these arguments to the `get.ps1` snippet.
+
 ```powershell
 powershell iex(irm https://raw.githubusercontent.com/he3als/EdgeRemover/main/get.ps1)
 ```
--- RemoveEdge.ps1
+++ RemoveEdge.ps1
@@ -1,14 +1,49 @@
-[CmdletBinding()]
+<#
+	.SYNOPSIS
+	Uninstalls Edge and Edge-related components.
+
+	.Description
+	Uninstalls Edge and Edge-related components in a non-forceful manner, based upon the switches or user choices.
+
+	.PARAMETER UninstallAll
+	Uninstalls everything, including WebView, Update, etc... without interaction, including Edge user data.
+
+	.PARAMETER UninstallEdge
+	Uninstalls Edge-only, leaving the Edgeuser data, without interaction.
+
+	.PARAMETER Exit
+	When combined with other parameters, this exits the script (even if there's errors) on completion.
+
+	.PARAMETER KeepAppX
+	Keeps the AppX, in case you want to use alternative AppX removal methods.
+
+	.LINK
+	https://github.com/he3als/EdgeRemover
+
+	.LINK
+	https://gist.github.com/ave9858/c3451d9f452389ac7607c99d45edecc6
+#>
+
 param (
-	[Switch]$UninstallAll,
-	[Switch]$Exit
+	[switch]$UninstallAll,
+	[switch]$UninstallEdge,
+	[switch]$Exit,
+	[switch]$KeepAppX
 )
+
+# credit to ave9858 for Edge removal method: https://gist.github.com/ave9858/c3451d9f452389ac7607c99d45edecc6
 
 $ProgressPreference = "SilentlyContinue"
 $user = $env:USERNAME
 $SID = (New-Object System.Security.Principal.NTAccount($user)).Translate([Security.Principal.SecurityIdentifier]).Value
 
-if ($Exit -and (-not $UninstallAll)) {
+$services = @(
+	'edgeupdate',
+	'edgeupdatem',
+	'MicrosoftEdgeElevationService'
+)
+
+if ($Exit -and ((-not $UninstallAll) -and (-not $UninstallEdge))) {
     $Exit = $false
 }
 
@@ -17,23 +52,46 @@ function PauseNul ($message = "Press any key to continue... ") {
 	$Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown') | Out-Null
 }
 
+function DeleteEdgeUpdate {
+	# surpress errors as some Edge Update components may not exist
+	$global:ErrorActionPreference = 'SilentlyContinue'
+
+	# disable automatic installation of Edge-related applications
+	$edgeupdatePath = "HKLM:\SOFTWARE\Policies\Microsoft\EdgeUpdate" | Out-Null
+	New-Item -Path $edgeupdatePath -Force | Out-Null
+	New-ItemProperty -Path "$edgeupdatePath" -Name "DoNotUpdateToEdgeWithChromium" -Type DWORD -Value 1 | Out-Null
+	New-ItemProperty -Path "$edgeupdatePath" -Name "Install{F3017226-FE2A-4295-8BDF-00C3A9A7E4C5}" -Type DWORD -Value 0 | Out-Null
+	New-ItemProperty -Path "$edgeupdatePath" -Name "InstallDefault" -Type DWORD -Value 0 | Out-Null
+
+	# remove scheduled tasks
+	Stop-Process -Name "MicrosoftEdgeUpdate" -Force | Out-Null
+	Remove-Item -Path "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Microsoft Edge Update" -Force | Out-Null
+	Unregister-ScheduledTask -TaskName "MicrosoftEdgeUpdateTaskMachineCore" -Confirm:$false | Out-Null
+	Unregister-ScheduledTask -TaskName "MicrosoftEdgeUpdateTaskMachineUA" -Confirm:$false | Out-Null
+
+	# remove all Edge services
+	foreach ($service in $services) {
+		Stop-Service -Name $service -Force
+		sc.exe delete $service | Out-Null
+	}
+
+	# delete the Edge Update folder
+	Remove-Item -Path "$env:SystemDrive\Program Files (x86)\Microsoft\EdgeUpdate" -Recurse -Force | Out-Null
+
+	# revert error action preference
+	$global:ErrorActionPreference = 'Continue'
+}
+
 function RemoveEdgeChromium {
 	$baseKey = "HKLM:\SOFTWARE\WOW6432Node\Microsoft"
-	
+
 	# kill Edge
 	$ErrorActionPreference = 'SilentlyContinue'
 
 	Get-Process -Name MicrosoftEdgeUpdate | Stop-Process -Force
 	Get-Process -Name msedge | Stop-Process -Force
-
-	$services = @(
-		'edgeupdate',
-		'edgeupdatem',
-		'MicrosoftEdgeElevationService'
-	)
-
 	foreach ($service in $services) {Stop-Service -Name $service -Force}
-	
+
 	$ErrorActionPreference = 'Continue'
 
 	# check if 'experiment_control_labels' value exists and delete it if found
@@ -57,11 +115,18 @@ function RemoveEdgeChromium {
 		$uninstallString = (Get-ItemProperty -Path $uninstallKeyPath).UninstallString + " --force-uninstall"
 		Start-Process cmd.exe "/c $uninstallString" -WindowStyle Hidden
 	}
-	
+
 	# remove user data
 	if ($removeData) {
-		$path = "$env:SystemDrive\Users\$user\AppData\Local\Microsoft\Edge"
+		$path = "$env:LOCALAPPDATA\Microsoft\Edge"
 		if (Test-Path $path) {Remove-Item $path -Force -Recurse}
+	}
+
+	# remove Edge shortcut on desktop
+	# may exist for some people after a proper uninstallation
+	$shortcutPath = "$env:USERPROFILE\Desktop\Microsoft Edge.lnk"
+	if (Test-Path $shortcutPath) {
+		Remove-Item $shortcutPath -Force
 	}
 }
 
@@ -93,17 +158,29 @@ function RemoveWebView {
 function UninstallAll {
 	Write-Warning "Uninstalling Edge Chromium..."
 	RemoveEdgeChromium
-	Write-Warning "Uninstalling AppX Edge..."
-	RemoveEdgeAppx
+	if (!($KeepAppX)) {
+		Write-Warning "Uninstalling AppX Edge..."
+		RemoveEdgeAppx
+	} else {Write-Warning "AppX Edge is being left, there might be a stub..."}
 	if ($removeWebView) {
 		Write-Warning "Uninstalling Edge WebView..."
 		RemoveWebView
+		Write-Warning "Uninstalling Edge Update..."
+		DeleteEdgeUpdate
 	}
 }
 
+function Completed {
+	Write-Host "`nCompleted." -ForegroundColor Green
+	if (!$Exit) {
+		PauseNul "Press any key to exit... "
+	}
+	exit
+}
+
 if ($null -ne $(whoami /user | Select-String "S-1-5-18")) {
-	Write-Host "This script can't be ran as TrustedInstaller or SYSTEM."
-	Write-Host "Please relaunch this script under a regular admin account.`n"
+	Write-Host "This script can't be ran as TrustedInstaller or SYSTEM. -ForegroundColor Yellow
+Please relaunch this script under a regular admin account.`n" -ForegroundColor Yellow
 	if (!($Exit)) {PauseNul "Press any key to exit... "}
 	exit 1
 } else {
@@ -112,48 +189,50 @@ if ($null -ne $(whoami /user | Select-String "S-1-5-18")) {
 	}
 }
 
-$removeWebView = $true
-$removeData = $true
-
-if ($Exit) {
+if ($UninstallAll) {
+	$removeWebView = $true
+	$removeData = $true
 	UninstallAll
-	exit
+	Completed
 }
 
-if (!($UninstallAll)) {
-	while (!($continue)) {
-		Clear-Host; Write-Host "This script will remove Microsoft Edge, as once you install it, you can't normally uninstall it.
-Major credit to ave9858: https://gist.github.com/ave9858/c3451d9f452389ac7607c99d45edecc6`n" -ForegroundColor Yellow
+if ($UninstallEdge) {
+	$removeWebView = $false
+	$removeData = $false
+	UninstallAll
+	Completed
+}
 
-		if ($removeWebView) {$colourWeb = "Green"; $textWeb = "Selected"} else {$colourWeb = "Red"; $textWeb = "Unselected"}
-		if ($removeData) {$colourData = "Green"; $textData = "Selected"} else {$colourData = "Red"; $textData = "Unselected"}
-		
-		Write-Host "Options:"
-		Write-Host "[1] Remove Edge WebView ($textWeb)" -ForegroundColor $colourWeb
-		Write-Host "[2] Remove Edge User Data ($textData)`n" -ForegroundColor $colourData
-		Write-Host "Press enter to continue or use numbers to select options... " -NoNewLine
-		
-		$userInput = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
-		
-		write-host "$input.VirtualKeyCode"
-		
-		switch ($userInput.VirtualKeyCode) {
-			49 { # num 1
-				$removeWebView = !$removeWebView
-			}
-			50 { # num 2
-				$removeData = !$removeData
-			}
-			13 { # enter
-				$continue = $true
-			}
+# set defaults
+$removeWebView = $false
+$removeData = $false
+
+while (!($continue)) {
+	Clear-Host; Write-Host "This script will remove Microsoft Edge, as once you install it, you can't normally uninstall it.`n" -ForegroundColor Yellow
+
+	if ($removeWebView) {$colourWeb = "Green"; $textWeb = "Selected"} else {$colourWeb = "Red"; $textWeb = "Unselected"}
+	if ($removeData) {$colourData = "Green"; $textData = "Selected"} else {$colourData = "Red"; $textData = "Unselected"}
+
+	Write-Host "Options:"
+	Write-Host "[1] Remove Edge WebView ($textWeb)" -ForegroundColor $colourWeb
+	Write-Host "[2] Remove Edge User Data ($textData)`n" -ForegroundColor $colourData
+	Write-Host "Press enter to continue or use numbers to select options... " -NoNewLine
+
+	$userInput = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
+
+	switch ($userInput.VirtualKeyCode) {
+		49 { # num 1
+			$removeWebView = !$removeWebView
+		}
+		50 { # num 2
+			$removeData = !$removeData
+		}
+		13 { # enter
+			$continue = $true
 		}
 	}
 }
 
 Clear-Host
 UninstallAll
-
-Write-Host "`nCompleted." -ForegroundColor Green
-PauseNul "Press any key to exit... "
-exit
+Completed
